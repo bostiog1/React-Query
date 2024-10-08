a1f79486db7813bf3264ac6dd63c8672d3f686b1
Teo <teo.bostiog@yahoo.com>
2024-10-08 15:45:55 +0300

    RQ: Fetching, errors, devtools,cache,refetch,pooling, useQuery onClick,

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,37 @@ import "./App.css";
 import { HomePage } from "./components/Home";
 import { SuperHeroes } from "./components/SuperHeroes";
 import { RQSuperHeroes } from "./components/RQSuperHeroes";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/super-heroes" element={<SuperHeroes />} />
-          <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroes />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQSuperHeroes.jsx
+++ src/components/RQSuperHeroes.jsx
@@ -1,3 +1,41 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroes = () => {
-  return <h2>React Query Super Heroes Page</h2>;
+  const onSuccess = (data) => {
+    console.log("Perform side effect after data fetching ", data);
+  };
+
+  const onError = (data) => {
+    console.log("Perform side effect after ecnoutnering error", data);
+  };
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes,
+    { onSuccess, onError }
+  );
+
+  console.log({ isLoading, isFetching });
+  if (isLoading || isFetching) {
+    return <h2>Loaging...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>;
+      <button onClick={refetch}>Fetch heroes</button>
+      {data?.data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </>
+  );
 };
